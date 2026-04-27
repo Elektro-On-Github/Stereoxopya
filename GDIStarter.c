8d12f3949a1c3b41da992a1c46db98a30689cd55
Elektro-On-Github <ElektroFromYT@gmail.com>
2026-04-27 18:59:53 +0200

    tunnel gdi

---
+++ GDIStarter.c
@@ -0,0 +1,3 @@
+#include <fx2GDI.c>
+#include <fxGDI.c>
+#include <fxTunnelGDI.c>
---
+++ fxTunnelGDI.c
@@ -0,0 +1,22 @@
+#include <windows.h>
+
+int main() {
+    HDC hdc = GetDC(NULL);
+
+    int w = GetSystemMetrics(SM_CXSCREEN);
+    int h = GetSystemMetrics(SM_CYSCREEN);
+
+    while(1) {
+        HDC hdc = GetDC(NULL);
+
+        int newW = w - (5 * w / 100);
+        int newH = h - (5 * h / 100);
+
+        int x = (w - newW) / 2;
+        int y = (h - newH) / 2;
+
+        StretchBlt(hdc, x, y, newW, newH, hdc, 0, 0, w, h, SRCCOPY);
+        ReleaseDC(NULL, hdc);
+        Sleep(100);
+    }
+}
